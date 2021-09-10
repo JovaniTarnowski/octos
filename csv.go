961d865bb9aefa9a72eb43596138738393f68ca6
M3gaZord <jovanitarnowski@gmail.com>
2021-09-10 17:36:59 +0000

    first commit

---
+++ csv.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"net/http"
+)
+
+func readCSVFromUrl(url string) ([][]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	reader := csv.NewReader(resp.Body)
+	reader.Comma = ';'
+	data, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func getByCNPJ(cpnj string) {
+
+	url := "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202009.csv"
+	data, err := readCSVFromUrl(url)
+	if err != nil {
+		panic(err)
+	}
+
+	for idx, row := range data {
+		//Find by CNPJ
+		if row[1] != cpnj {
+			continue
+		}
+
+		if idx == 6 {
+			break
+		}
+
+		fmt.Println(row)
+	}
+}
+
+func main() {
+	getByCNPJ("00.017.024/0001-53")
+}
